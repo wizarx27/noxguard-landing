e31fcf49ab47151eaf04527a6e8b777a1ca844bd
wizarx27 <ordon27e@gmail.com>
2023-03-18 22:24:46 +0700

    fix: design and laytout

--- src/App.tsx
+++ src/App.tsx
@@ -32,10 +32,16 @@ function App() {
     window.innerHeight,
   ]);
 
+  const [pentestOpen,setPentestOpen] = useState(false)
+  const [employeeTraining,setemployeeTraining] = useState(false)
+  const [isoOpen,setIsoOpen] = useState(false)
+  const [academy,setAcademy] = useState(false)
+
   useEffect(() => {
     const handleWindowResize = () => {
       setWindowSize([window.innerWidth, window.innerHeight]);
     };
+    document.title = "Nox Guard"
 
     window.addEventListener('resize', handleWindowResize);
 
@@ -43,14 +49,15 @@ function App() {
       window.removeEventListener('resize', handleWindowResize);
     };
   },[]);
+
   return (
     <div className="App">
       <Navbar bg="light" expand="lg" style={{width:windowSize[0]-15}}>
         {/* {windowSize[0]}
         <br />
         {windowSize[1]} */}
-        <Container>
-          <Navbar.Brand href="#home"><Image src={logoNavbar} width={50} height={50}/></Navbar.Brand>
+        <div className={'m-0 d-flex flex-grow-1 px-3' + (windowSize[0] < 992 ? " justify-content-between" : "")}>
+          <Navbar.Brand href="/"><Image src={logoNavbar} height={40}/></Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto nav-container">
@@ -66,28 +73,32 @@ function App() {
               <Image src={signInButton} height={30} width={80}/>
             </Nav>
           </Navbar.Collapse>
-        </Container>
+        </div>
       </Navbar>
-      <div className='section' style={{height:windowSize[1]-100,width:windowSize[0]-15}}>
+      <div className='section' style={{height:windowSize[0] < 1320 ? windowSize[1] * 2 -100 : windowSize[1]-100,width:windowSize[0]-15}}>
         <Image src={banner1} className='banner-bg'/>
+        <div className='header-gap mb-3'></div>
         <Row className='w-100 h-100 m-0'>
-          <Col xxl="6" xl="6" lg="6" md="6" sm="6" xs="6" className='p-0'>
+          <Col xxl="6" xl="6" lg="12" md="12" sm="12" xs="12" className='p-0'>
+            <Image src={logoAboutUs} className='img-about-us my-5'/>
           </Col>
-          <Col xxl="6" xl="6" lg="6" md="6" sm="6" xs="6" className='p-0 h-100'>
+          <Col xxl="6" xl="6" lg="12" md="12" sm="12" xs="12" className='p-0 h-100'>
             <div className='d-flex flex-column banner-text'>
-              <h2 className='banner-title'>
-                RAISING THE BAR FOR CYBER SECURITY,
+              <h2 className={`banner-title ${windowSize[0] < 1320 ? "banner-title-sm" : ""}`}>
+                RAISING THE BAR 
+                <br />
+                FOR CYBER SECURITY,
                 <br />
                 <span className='green-text'> ONE AUDIT AT A TIME.</span>
               </h2>
               {/* <p></p> */}
-              <div>
-                <Image src={startNowButton}/>
-                <Button className='ms-2 started-btn'>Getting Started</Button>
+              <div className={`pt-5 ${windowSize[0] < 1320 ? "mx-auto" : ""}`}>
+                <Image src={startNowButton} className='start-now-btn'/>
               </div>
             </div>
           </Col>
         </Row>
+        <div className='header-gap-bot mb-3'></div>
       </div>
       <div className='separator'></div>
       <div className='section section-2' >
@@ -107,34 +118,36 @@ function App() {
         <Row className='w-100 h-100 m-0'>
           <Col xxl="12" xl="12" className='p-0'>
               <div className='section-3-content'>
-                <h2 className='text-align-center my-3'>OUR VALUES</h2>
+                <h2 className='text-align-center my-3 our-values-title mt-5'>OUR VALUES</h2>
                 <div className='d-flex section-3-content-content justify-content-center flex-wrap'>
-                  <div className='d-flex flex-column align-items-center'>
-                    <Image src={IcnFast} width={305} height={305}/>
+                  <div className='d-flex flex-column align-items-center mb-5'>
+                    <Image src={IcnPrice} width={305} height={305}/>
                     <h3>AFFORDABLE PRICING</h3>
                     <p>
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                      We offer a cheaper price than our competitors for our services.
                     </p>
                   </div>
                   <div className='d-flex flex-column align-items-center'>
-                    <Image src={IcnPrice} width={305} height={305}/>
+                    <Image src={IcnFast} width={305} height={305}/>
                     <h3>FAST AND ACCURATE</h3>
                     <p>
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                      Our service is characterized by a speedy and precise performance. 
                     </p>
                   </div>
                   <div className='d-flex flex-column align-items-center'>
-                    <Image src={IcnQuality} width={305} height={305} style={{padding:"45px"}}/>
+                    <Image src={IcnQuality} width={305} height={305}/>
                     <h3>QUALITY</h3>
                     <p>
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                      Our clients guarantee and praise the quality of our service.
                     </p>
                   </div>
                 </div>
-                <div className='text-align-center my-5 about-us-below'>
+                <div className='separator tiny'></div>
+                <div className='text-align-center about-us-below'>
                   PENETRATION TESTING AND AUDIT HELPS TO IDENTIFYING <br />
                   SECURITY VULNERABILITIES BEFORE MALICIOUS ATTACKS CAN OCCUR
                 </div>
+                <div className='separator tiny'></div>
               </div>
           </Col>
         </Row>
@@ -142,14 +155,14 @@ function App() {
       <div className='section section-4 pb-5' >
         <Row className='w-100 h-100 m-0'>
           <Col xxl="12" xl="12" className='h-100'>
-              <Image src={logoAboutUs} className='img-about-us'/>
+              <Image src={logoAboutUs} className='img-about-us my-5'/>
           </Col>
         </Row>
 
         <Row className='w-100 h-100 m-0'>
           <Col xxl="12" xl="12" className='h-100'>
-              <div className='ms-5 section-4-content'>
-                <h2>About us</h2>
+              <div className={`ms-5 ${windowSize[0] < 992 ? "section-4-content-sm" : "section-4-content"}`}>
+                <h2 className='about-us-title'>ABOUT US</h2>
                 <p>
                   Nox Guard is a highly skilled and motivated team focused on cybersecurity audit services such as ISO 27001 compliance and penetration testing, as well as education. We aim to identify vulnerabilities and address them before they can be exploited, and we're committed to sharing knowledge through training and consultation to promote a secure environment that inspires confidence in your business operations. With an ever-evolving nature of cyber threats, we stay up-to-date with the latest techniques and technologies to deliver innovative and effective solutions to our clients. Trust us to be your cybersecurity partner and help you secure your digital assets, educate your team, and ensure your peace of mind. Contact us today to learn more.
                 </p>
@@ -158,89 +171,90 @@ function App() {
         </Row>
         
       </div>
+      <div className='separator tiny'></div>
       <div className='section section-5' >
         <Row className='w-100 h-100 m-0'>
           <Col xxl="12" xl="12" className='h-100 my-5 mx-0'>
-          <h2 className='text-align-center'>OUR SERVICES</h2>
+          <h2 className='text-align-center our-service-title'>OUR SERVICES</h2>
           </Col>
         </Row>
-        <Row className='w-100 h-100 m-0 my-5'>
+        <Row className={`w-100 h-100 m-0 ${windowSize[0] < 1200 ? "mt-5" : "my-5"} `}>
           <Col xxl="6" xl="6" className='h-100'>
-              <div>
+              <div onMouseEnter={()=>{setPentestOpen(true)}} onMouseLeave={()=>{setPentestOpen(false)}} className='d-flex align-items-center'>
                 <Image src={decor3} width={20} height={20}/>
                 <span className='ms-2 our-service-content-title'>Penetration Testing</span>
               </div>
-              <p className='margin-offset'>
+              <p className={`margin-offset service-description ${pentestOpen ? "active" : ""} `}>
                 Penetration testing is a thorough examination of your web and mobile applications,databases, infrastructure, and networks to assess their security posture
               </p>              
           </Col>
           <Col xxl="6" xl="6" className='h-100'>
-              <div>
+              <div onMouseEnter={()=>{setemployeeTraining(true)}} onMouseLeave={()=>{setemployeeTraining(false)}} className='d-flex align-items-center'>
                 <Image src={decor3} width={20} height={20}/>
                 <span className='ms-2 our-service-content-title'>Employee Training</span>
               </div>
-              <p className='margin-offset'> 
+              <p className={`margin-offset service-description ${employeeTraining ? "active" : ""} `}> 
                 Our training program is designed to increase your employees security awareness through social engineering, briefings, and phishing simulations. This is crucial because humans are often the weakest link in cybersecurity.
               </p>  
           </Col>
         </Row>
-        <Row className='w-100 h-100 m-0 my-5'>
+        <Row className={`w-100 h-100 m-0 ${windowSize[0] < 1200 ? "mb-5" : "my-5"} `}>
           <Col xxl="6" xl="6" className='h-100'>
-              <div>
+              <div onMouseEnter={()=>{setIsoOpen(true)}} onMouseLeave={()=>{setIsoOpen(false)}} className='d-flex align-items-center'>
                 <Image src={decor3} width={20} height={20}/>
                 <span className='ms-2 our-service-content-title'>ISO 27001</span>
               </div>
-              <p className='margin-offset'>
+              <p className={`margin-offset service-description ${isoOpen ? "active" : ""} `}>
                 ISO 27001 and NIST assessment ensures that your organization compiles with the iso 27001 and NIST standards
               </p>              
           </Col>
           <Col xxl="6" xl="6" className='h-100'>
-              <div>
+              <div onMouseEnter={()=>{setAcademy(true)}} onMouseLeave={()=>{setAcademy(false)}} className='d-flex align-items-center' >
                 <Image src={decor3} width={20} height={20}/>
-                <span className='ms-2 our-service-content-title'>Penetration Testing</span>
+                <span className='ms-2 our-service-content-title'>Academy</span>
               </div>
-              <p className='margin-offset'>
+              <p className={`margin-offset service-description ${academy ? "active" : ""} `}>
                 Our Academy offers a comprehensize range of technical classes, from beginner to advanced levels, designed to enhance your knowledge and expertise in cybersecurity
               </p>  
           </Col>
         </Row>
       </div>
-
+      <div className='separator tiny'></div>
       <div className='section section-6' >
         <Row className='w-100 h-100 m-0'>
           <Col xxl="12" xl="12" className='h-100 my-5 mx-0'>
-              <h2 className='text-align-center'>THE TEAM</h2>
+              <h2 className='text-align-center the-team-title'>THE TEAM</h2>
           </Col>
         </Row>
         <Row className='w-100 h-100 m-0 my-5'>
-          <Col xxl="12" xl="12" className='h-100 d-flex justify-content-center team-row'>
+          <Col xxl="12" xl="12" className='h-100 d-flex justify-content-center team-row flex-wrap'>
               <div className='d-flex flex-column align-items-center img-team'>
-                <Image src={imageTeam1} height={150} width={150}/>
+                <Image src={imageTeam1} width={250}/>
                 <h3 className='text-align-center team-name'>Ardian Danny</h3>
                 <p className='text-align-center'>CEO & Penetration Tester</p>
               </div>
               <div className='d-flex flex-column align-items-center img-team'>
-                <Image src={imageTeam2} height={150} width={150}/>
+                <Image src={imageTeam2} width={250}/>
                 <h3 className='text-align-center team-name'>Felix Alexander</h3>
                 <p className='text-align-center'>Lead Penetration Tester & Researcher</p>
               </div>
               <div className='d-flex flex-column align-items-center img-team'>
-                <Image src={imageTeam3} height={150} width={150}/>
-                <h3 className='text-align-center team-name'>Delbert Gioavanni Lie</h3>
+                <Image src={imageTeam3} width={250}/>
+                <h3 className='text-align-center team-name'>Delbert Giovanni Lie</h3>
                 <p className='text-align-center'>Penetration Tester</p>
               </div>
 
           </Col>
         </Row>
         <Row className='w-100 h-100 m-0'>
-          <Col xxl="12" xl="12" className='h-100 d-flex justify-content-center team-row'>
+          <Col xxl="12" xl="12" className='h-100 d-flex justify-content-center team-row flex-wrap'>
               <div className='d-flex flex-column align-items-center img-team'>
-                <Image src={imageTeam4} height={150} width={150}/>
+                <Image src={imageTeam4} width={250}/>
                 <h3 className='text-align-center team-name'>Stanley Halim</h3>
                 <p className='text-align-center'>Penetration Tester</p>
               </div>
               <div className='d-flex flex-column align-items-center img-team'>
-                <Image src={imageTeam5} height={150} width={150}/>
+                <Image src={imageTeam5} width={250}/>
                 <h3 className='text-align-center team-name'>Crisdeo Nuel Silahan</h3>
                 <p className='text-align-center'>Penetration Tester</p>
               </div>
